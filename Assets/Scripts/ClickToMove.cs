4d3cc1d5a9119ae66a5e17a9a859c147cf2cdd3f
Brad <Brad@CHAMPION>
2017-05-11 01:26:01 -0400

    V0.2 -Click to move rotation is working -Changed player to disc shape -Added target for cursor

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -5,8 +5,12 @@ using System.Collections;
 public class ClickToMove : MonoBehaviour
 {
 
-    public Transform sphere;
-    public float rotSpeed;
+    public Transform playerShip,cursor;
+    private float targetLat;
+    private float targetLon;
+    private float currentLat;
+    private float currentLon;
+    public float moveSpeed;
 
     // Use this for initialization
     void Awake()
@@ -17,16 +21,46 @@ public class ClickToMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        // perform raycast to get a point on the sphere
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (GetComponent<Collider>().Raycast(ray, out hit, float.PositiveInfinity))
+        {
+            // draw a line in the editor to visualize the hit point
+            Debug.DrawRay(hit.point, hit.normal, Color.yellow);
+            
+            // convert the hit point into local coordinates
+            Vector3 localPos = transform.InverseTransformPoint(hit.point);
+            Vector3 longDir = localPos;
+            // zero y to project the vector to the x-z-plane
+            longDir.y = 0;
 
-    private void OnMouseDrag()
-    {
-        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
-        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+            //calculate the angle between our reference and the hitpoint
+            float longitude = Vector3.Angle(-Vector3.forward, longDir);
+            // if our point is on the western hemisphere negate the angle
+            if (longDir.x < 0)
+                longitude = -longitude;
+            // calculate the latitude in degree
+            float latitude = Mathf.Asin(localPos.normalized.y) * Mathf.Rad2Deg;
 
-        transform.Rotate(Vector3.up, -rotX);
-        transform.Rotate(Vector3.right, rotY);
+            cursor.position = hit.point;
+            if (Input.GetMouseButtonDown(0))
+            {
+                targetLat = latitude;
+                targetLon = longitude;
+
+            }
+        }
+
+        // slowly move the currentLat / Lon towards our targetLat / Lon
+        currentLat = Mathf.LerpAngle(currentLat, targetLat, Time.deltaTime * moveSpeed);
+        currentLon = Mathf.LerpAngle(currentLon, targetLon, Time.deltaTime * moveSpeed);
+
+        // build our rotation from the two angles
+        transform.localRotation =
+            Quaternion.AngleAxis(-currentLat, Vector3.right) *
+            Quaternion.AngleAxis(currentLon, Vector3.up);
+        playerShip.transform.rotation = Quaternion.LookRotation(hit.point, Vector3.back);
     }
 
 }
